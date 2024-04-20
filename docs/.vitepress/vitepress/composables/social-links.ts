e225a5e77b3b87a9b94f83db9694b8a3c334e889
wangxiang <1827945911@qq.com>
2024-04-20 11:32:10 +0800

    fix: cursor link

--- docs/.vitepress/vitepress/composables/social-links.ts
+++ docs/.vitepress/vitepress/composables/social-links.ts
@@ -3,7 +3,7 @@ import GitHubIcon from '~icons/ri/github-fill'
 export const useSocialLinks = () => {
   return [
     {
-      link: 'https://github.com/wangxiang4/dolphin-ecology-docs',
+      link: 'https://github.com/wangxiang4/mba-exam-docs',
       icon: GitHubIcon,
       text: 'GitHub',
     },
