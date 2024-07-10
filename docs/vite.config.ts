1c55010e0e13e6653c6ffab52607eb7579f0bff4
wangxiang <1827945911@qq.com>
2024-07-10 23:03:11 +0800

    compose: resource

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -6,6 +6,7 @@ import Icons from 'unplugin-icons/vite'
 import IconsResolver from 'unplugin-icons/resolver'
 import UnoCSS from 'unocss/vite'
 import { presetUno, presetIcons, presetAttributify  } from 'unocss'
+import { photoSwipePlugin } from '@vuepress/plugin-photo-swipe'
 
 export default defineConfig({
   resolve: {
@@ -81,6 +82,8 @@ export default defineConfig({
       ]
     }),
 
-    MarkdownTransform()
+    MarkdownTransform(),
+
+    photoSwipePlugin
   ]
 })
